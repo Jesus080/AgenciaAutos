436263f8715c937537b220a73201772d97678a94
Jesus Alberto Gonzalez Sanchez <135742583+Jesus080@users.noreply.github.com>
2024-02-27 07:07:12 -0600

    Update autos.js

--- js/autos.js
+++ js/autos.js
@@ -68,55 +68,5 @@ const autoTDA =(marca,precio,origen,nombreComprador) =>{
           "Precio Final $: " + aumentoOirgen45Iva16;
 
 }
-/*function autosTDA(marca,precio,origen,nombreComprador){
-   let aumentoOrigen=0;
-   let aumentoOrigen45=0;
-   let aumentoOirgen45Iva16=0;
-   let aumentoOrigFinal=0;
-   let aumento30=0;
-
-   if(origen == "Alemania"){
-         aumentoOrigen45 = (precio*0.45)
-         aumentoOrigen = (precio + aumentoOrigen45)
-         aumento30 = (aumentoOrigen*0.20)
-         aumentoOrigFinal = aumentoOrigen + aumento30
-   }else if(origen == "Japon"){
-      aumentoOrigen45 = (precio*0.45)
-         aumentoOrigen = (precio + aumentoOrigen45)
-         aumento30 = (aumentoOrigen*0.30)
-         aumentoOrigFinal = aumentoOrigen + aumento30
-   }
-   else if(origen == "Italia"){
-      aumentoOrigen45 = (precio*0.45)
-      aumentoOrigen = (precio + aumentoOrigen45)
-      aumento30 = (aumentoOrigen*0.15)
-      aumentoOrigFinal = aumentoOrigen + aumento30
-   }
-   else if(origen == "EU"){
-      aumentoOrigen45 = (precio*0.45)
-      aumentoOrigen = (precio + aumentoOrigen45)
-      aumento30 = (aumentoOrigen*0.08)
-      aumentoOrigFinal = aumentoOrigen + aumento30
-
-      
-   }else if(origen == "noSelecciona"){
-      alert("No selecciono Origen");
-   }
-    aumentoOirgen45Iva16 =aumentoOrigFinal+(aumentoOrigFinal*0.16)
-    
-     return  "Nombre del comprador: " + nombreComprador  +"::::::::::"+
-             "Marca del automovil: " + marca +"::::::::::"+
-             "Origen del automvil: " + origen  +"::::::::"+
-             "Precio Principal $:" + precio +"::::::::::::"+
-             "Precio sin IVA $:" + aumentoOrigFinal +":::::::::::"+
-             "Precio Final $: " + aumentoOirgen45Iva16 ;
-
-             
-            
-
-
-             
-}*/
-
 
 
